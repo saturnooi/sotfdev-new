50db313eba146399b3823e8bcd774a8d4bd2a793
์Nuttawat <83624530+saturnooi@users.noreply.github.com>
2021-11-15 15:14:01 +0700

    f commit

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <navber/>
+  </div>
+</template>
+
+<script>
+
+import navber from '../components/navber.vue'
+
+export default {
+  components: {
+    navber,
+  }
+}
+</script>
